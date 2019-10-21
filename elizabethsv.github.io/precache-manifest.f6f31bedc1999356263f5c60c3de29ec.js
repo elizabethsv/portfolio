7c4a5afe913c8d1ab3901ef3a31ef072b1b44a73
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-10-21 13:00:25 -0500

    netlify

---
+++ elizabethsv.github.io/precache-manifest.f6f31bedc1999356263f5c60c3de29ec.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24316f6424a4c7ebc59e9704ed9037df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704e836438e775297d26",
+    "url": "/static/css/main.d98f82a6.chunk.css"
+  },
+  {
+    "revision": "c953a4046a0b893b366c",
+    "url": "/static/js/2.b0e2ff7f.chunk.js"
+  },
+  {
+    "revision": "704e836438e775297d26",
+    "url": "/static/js/main.2c33a810.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  },
+  {
+    "revision": "216be6779b9f97d8c2a7df68d01e49df",
+    "url": "/static/media/PT_thumb.216be677.png"
+  },
+  {
+    "revision": "b19e262783c150b316cf343a8d344fee",
+    "url": "/static/media/brushstroke.b19e2627.png"
+  },
+  {
+    "revision": "4a34682aa5ebdefec3fbedd2e1d4ed11",
+    "url": "/static/media/brushstroke2.4a34682a.jpg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/static/media/expressjs.0fb30b49.svg"
+  },
+  {
+    "revision": "ce3847fe6a52d2dac59e7117170c9ed2",
+    "url": "/static/media/javascript.ce3847fe.svg"
+  },
+  {
+    "revision": "d571053a3cda81b67bfdaee97caab9bc",
+    "url": "/static/media/mongo.d571053a.svg"
+  },
+  {
+    "revision": "5c376133d6403088a87a04fcc9685674",
+    "url": "/static/media/nodejs.5c376133.svg"
+  },
+  {
+    "revision": "94cf7fd8d98b323e26563eb5b49d8203",
+    "url": "/static/media/photoshop.94cf7fd8.svg"
+  },
+  {
+    "revision": "24e4df4913b9c5bdea0d180641868c3b",
+    "url": "/static/media/postgresql.24e4df49.svg"
+  },
+  {
+    "revision": "1b0d20fd7b874c87cfc05675abbeabb5",
+    "url": "/static/media/purple_thumb.1b0d20fd.png"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/static/media/reactjs-icon.70384772.svg"
+  },
+  {
+    "revision": "5f87aa1a7aeffddf55b8b723679a6779",
+    "url": "/static/media/sass.5f87aa1a.svg"
+  },
+  {
+    "revision": "f62155144a97d8c3cb0db952b6a62215",
+    "url": "/static/media/temp.f6215514.png"
+  },
+  {
+    "revision": "2618d5e82d87079d654074ae5aa8bc93",
+    "url": "/static/media/thumbnail.2618d5e8.jpg"
+  },
+  {
+    "revision": "dac0700b8a7959490dd1f20c67cfce8e",
+    "url": "/static/media/ticket_thumb.dac0700b.png"
+  },
+  {
+    "revision": "c36e8d0e6e073de5703e8f2fb19efe5b",
+    "url": "/static/media/watercolour.c36e8d0e.jpg"
+  }
+]);
