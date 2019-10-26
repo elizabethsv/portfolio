cdec8d9b2484b52c56f1425fdaaef63aeb35c0e1
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-10-26 17:31:35 -0500

    added keto

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,7 @@ import purple_thumb from '../images/purple_thumb.png'
 import streetfighter from '../images/thumbnail.jpg'
 import ticketthumb from '../images/ticket_thumb.png'
 import PT_thumb from '../images/PT_thumb.png'
+import keto from '../images/keto-ai.png'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
@@ -11,7 +12,20 @@ export const Portfolio = () =>{
     return(
         <Wrapper>
             <div id='title'>Portfolio</div>
+            
+            <div id="featured">
+                
+                <img src={keto} style={{width:'50%', boxShadow:'0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'}} alt='ketoai screenshot'/>
+                <div id="featured-desc">
+                    <h4>Keto-AI Web App</h4>
+                    <p>As part of the <a href="http://keto-ai.com">Keto-AI</a>'s team, I was responsible for contributing to adding features to their web application.</p>
+                    
+                </div>
+                
+            </div>
+            
         <div className="portfolio" id="portfolio">
+            
                 <div className='thumbnail'>
                     <Link to="/purplemap">
                         <img src={purple_thumb} alt="Purple Map App Thumbnail"/>
@@ -40,9 +54,9 @@ export const Portfolio = () =>{
                 </div>
                     
                 <div className='thumbnail'>
-                    <img src={PT_thumb} alt="PT Client Management Thumbnail"/>
+                    <img src={PT_thumb} alt="Client Management Thumbnail"/>
                     <div className='overlay'>
-                        <div className='text'>PT Client Management</div>
+                        <div className='text'>Client Management</div>
                     </div>
                 </div>
 
